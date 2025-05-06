0687190a5f496a73cfed57c2bd608b1d897b0631
Alexis <alexisgioda@gmail.com>
2025-05-06 22:04:46 +0200

    create mint account on client and msg return on program

--- code/client/src/main.rs
+++ code/client/src/main.rs
@@ -1,7 +1,14 @@
 use std::{error::Error, str::FromStr};
 
 use solana_client::rpc_client::RpcClient;
-use solana_sdk::{instruction::Instruction, native_token::LAMPORTS_PER_SOL, pubkey::Pubkey, signature::{Keypair, Signature}, signer::Signer, transaction::Transaction};
+use solana_sdk::{
+    instruction::{AccountMeta, Instruction},
+    native_token::LAMPORTS_PER_SOL,
+    pubkey::Pubkey,
+    signature::{Keypair, Signature, Signer},
+    system_instruction,
+    transaction::Transaction,
+};
 
 pub fn create_keypair() -> Keypair {
     Keypair::new()
@@ -26,11 +33,54 @@ pub fn check_balance(rpc_client: &RpcClient, pubkey: &Pubkey) -> Result<f64, Box
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let rpc_client = RpcClient::new("http://localhost:8899");
+    let rpc_client = RpcClient::new("http://localhost:8899"); // interagir avec le noeud solana
+    let fees_payer = create_keypair(); // celui qui va payer les frais
+    let mint_account = create_keypair(); // le compte de mint
 
+    // Program id
+    let program_id = Pubkey::from_str("FWEKEyaEyxeceM8Tr8YLMRmJsL9UwYejyeDMp7e8aJTn")?;
+
+    // Fees Airdrop
+    request_airdrop(&rpc_client, &fees_payer.pubkey(), 3.0)?; // 3 SOL
+    println!("Fees Payer balance: {:.2}", check_balance(&rpc_client, &fees_payer.pubkey())?); // affiche le solde du fees payer
+
+    // eviter que le compte soit supprimer pcq y a pas assez de SOL
+    let mint_account_space = 8; // taille minimale (en octet) pour un compte solana
+    let mint_account_lamports = rpc_client.get_minimum_balance_for_rent_exemption(mint_account_space)?; // calcul le nombre minimal de Lamports pour eviter le loyer
+
+    // instruction pour le mint
+    let create_account_ix = system_instruction::create_account(
+        &fees_payer.pubkey(),
+        &mint_account.pubkey(),
+        mint_account_lamports,
+        mint_account_space as u64,
+        &Pubkey::from_str("FWEKEyaEyxeceM8Tr8YLMRmJsL9UwYejyeDMp7e8aJTn")?, // ID du programme
+    );
+
+    // construction du mint_instruction
+    let mint_instruction = Instruction {
+        program_id,
+        accounts: vec![
+            AccountMeta::new(fees_payer.pubkey(), true), // fees_payer = signer
+            AccountMeta::new(mint_account.pubkey(), false), // mint_account = cible du mint
+        ],
+        data: vec![1], // on envoie juste 1 byte: 1 → qui correspond à l’instruction mint
+    };
     
-
+    // creation de l'instruction
+    let mut transaction = Transaction::new_with_payer(
+        &[create_account_ix, mint_instruction],
+        Some(&fees_payer.pubkey()),
+    );
     
-
+    // signature par celui qui paye les frais
+    let recent_blockhash = rpc_client.get_latest_blockhash()?;
+    transaction.sign(&[&fees_payer, &mint_account], recent_blockhash);
+    
+    // confirmation de la signature par le reseau
+    let signature = rpc_client.send_and_confirm_transaction(&transaction)?;
+    println!("✅ Transaction sent! Signature: {}", signature);
+    
     Ok(())
+
 }
--- code/program/src/lib.rs
+++ code/program/src/lib.rs
@@ -1,24 +1,39 @@
 use solana_program::{
-    account_info::{AccountInfo, next_account_info},
+    account_info::{next_account_info, AccountInfo},
     entrypoint,
     entrypoint::ProgramResult,
     msg,
     pubkey::Pubkey,
+    program_error::ProgramError,
 };
 
 entrypoint!(process_instruction);
 
 fn process_instruction(
-    program_id: &Pubkey,
+    _program_id: &Pubkey,
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
     let accounts_iter = &mut accounts.iter();
-    let fees_payer = next_account_info(accounts_iter)?;
+    let _fees_payer = next_account_info(accounts_iter)?;
+    let mint_account = next_account_info(accounts_iter)?;
 
-    // 1 → mint
+    // l'instruction '1' → mint
     if instruction_data[0] == 1 {
-        msg!("Minting 200 tokens to {}", fees_payer.key);
+        msg!("Minting 200 tokens to {}", mint_account.key);
+
+        // On encode 200 comme u64 et on l’écrit dans le data du compte
+        let amount: u64 = 200;
+        let amount_bytes = amount.to_le_bytes();
+
+        // Vérifie que le compte est writable
+        if !mint_account.is_writable {
+            return Err(ProgramError::InvalidAccountData);
+        }
+
+        // On écrit dans le compte
+        let data = &mut *mint_account.try_borrow_mut_data()?;
+        data[..8].copy_from_slice(&amount_bytes);
     }
 
     Ok(())
